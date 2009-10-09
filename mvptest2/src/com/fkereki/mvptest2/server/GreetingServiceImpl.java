0df6dca79186158322b4d9d9d2d013fa2518045e
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2009-10-09 09:51:20 +0000

    Outgoing #2

---
+++ mvptest2/src/com/fkereki/mvptest2/server/GreetingServiceImpl.java
@@ -0,0 +1,22 @@
+package com.fkereki.mvptest2.server;
+
+import com.fkereki.mvptest2.client.GreetingService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends
+    RemoteServiceServlet implements GreetingService {
+
+  public String greetServer(String input) {
+    String serverInfo = getServletContext().getServerInfo();
+    String userAgent = getThreadLocalRequest().getHeader(
+      "User-Agent");
+    return "Hello, " + input + "!<br><br>I am running "
+      + serverInfo
+      + ".<br><br>It looks like you are using:<br>"
+      + userAgent;
+  }
+}
