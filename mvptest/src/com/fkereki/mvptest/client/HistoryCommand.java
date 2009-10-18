0841f7dd9d3f8c67d362eba432e69335dc859393
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2009-10-18 03:52:42 +0000

    Separate HistoryCommand class, History processing.

---
+++ mvptest/src/com/fkereki/mvptest/client/HistoryCommand.java
@@ -0,0 +1,16 @@
+package com.fkereki.mvptest.client;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.History;
+
+public class HistoryCommand implements Command {
+  String historyToken;
+
+  public HistoryCommand(final String newToken) {
+    historyToken = newToken;
+  }
+
+  public void execute() {
+    History.newItem(historyToken, true);
+  }
+}
--- mvptest/src/com/fkereki/mvptest/client/Mvptest.java
+++ mvptest/src/com/fkereki/mvptest/client/Mvptest.java
@@ -1,68 +1,48 @@
 package com.fkereki.mvptest.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Command;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class Mvptest implements EntryPoint {
+public class Mvptest implements EntryPoint,
+    ValueChangeHandler<String> {
+  final VerticalPanel runPanel = new VerticalPanel();
+
   public void onModuleLoad() {
     MenuBar runMenuBar = new MenuBar();
-    final VerticalPanel runPanel = new VerticalPanel();
     Grid rootDisplay = new Grid(2, 1);
-    Command cmd = new Command() {
-      public void execute() {
-        runPanel.add(new Label("xxx"));
-      }
-    };
-
-    Command cmd2 = new Command() {
-      public void execute() {
-        runPanel.clear();
-        runPanel.add(new FormTwoLabels());
-      }
-    };
-
-    Command cmd3 = new Command() {
-      public void execute() {
-        runPanel.clear();
-        runPanel.add(new FormManyFields());
-      }
-    };
 
     runMenuBar.setWidth("100%");
-    runMenuBar.addItem("the", cmd);
-    runMenuBar.addItem("foo", cmd2);
-    runMenuBar.addItem("menu", cmd3);
-
-    MenuBar fooMenu = new MenuBar(true);
-    fooMenu.addItem("the", cmd);
-    fooMenu.addItem("foo", cmd);
-    fooMenu.addItem("menu", cmd);
-
-    MenuBar barMenu = new MenuBar(true);
-    barMenu.addItem("the", cmd);
-    barMenu.addItem("bar", cmd);
-    barMenu.addItem("menu", cmd);
-
-    MenuBar bazMenu = new MenuBar(true);
-    bazMenu.addItem("the", cmd);
-    bazMenu.addItem("baz", cmd);
-    bazMenu.addItem("menu", cmd);
-
-    // Make a new menu bar, adding a few cascading menus to
-    // it.
-    runMenuBar.addItem("foo", fooMenu);
-    runMenuBar.addItem("bar", barMenu);
-    runMenuBar.addItem("baz", bazMenu);
+    runMenuBar.addItem("the", new HistoryCommand("foo"));
+    runMenuBar.addItem("foo", new HistoryCommand("bar"));
+    runMenuBar.addItem("menu", new HistoryCommand("baz"));
 
     rootDisplay.setWidth("100%");
     rootDisplay.setWidget(0, 0, runMenuBar);
     rootDisplay.setWidget(1, 0, runPanel);
 
     RootPanel.get().add(rootDisplay);
+    History.addValueChangeHandler(this);
+  }
+
+  @Override
+  public void onValueChange(ValueChangeEvent<String> event) {
+    runPanel.clear();
+    String token = event.getValue();
+    if (token.equals("foo")) {
+      runPanel.add(new FormTwoLabels());
+    } else if (token.equals("bar")) {
+      runPanel.add(new FormManyFields());
+    } else if (token.equals("baz")) {
+      runPanel.add(new Label("just bazzing along..."));
+    } else if (token.equals("")) {
+      // no need to do anything...
+    }
   }
 }
