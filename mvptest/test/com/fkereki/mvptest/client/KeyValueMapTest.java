457e6fff18e981fd9476d882c6a20983ec13d2f1
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2009-10-25 03:56:45 +0000

    First unit test.

---
+++ mvptest/test/com/fkereki/mvptest/client/KeyValueMapTest.java
@@ -0,0 +1,52 @@
+package com.fkereki.mvptest.client;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KeyValueMapTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void testKeyValueMap() {
+    KeyValueMap kvm0 = new KeyValueMap("");
+    assertTrue(kvm0.isEmpty());
+  }
+
+  @Test
+  public void testKeyValueMapString() {
+    KeyValueMap kvm2 = new KeyValueMap("pepe=123&juan=456");
+    assertEquals(2, kvm2.size());
+    assertTrue(kvm2.containsKey("pepe"));
+    assertTrue(kvm2.containsKey("juan"));
+    assertEquals("123", kvm2.get("pepe"));
+    assertEquals("456", kvm2.get("juan"));
+  }
+
+  @Test
+  public void testToString() {
+    KeyValueMap kvm0 = new KeyValueMap("");
+    assertEquals("", kvm0.toString());
+
+    KeyValueMap kvm1 = new KeyValueMap("pepe=123456");
+    assertEquals("pepe=123456", kvm1.toString());
+
+    KeyValueMap kvm2 = new KeyValueMap(
+      "pepe=123&juan=456&luis=7890");
+    String kvmst2 = kvm2.toString();
+    assertTrue(kvmst2.contains("pepe=123"));
+    assertTrue(kvmst2.contains("juan=456"));
+    assertTrue(kvmst2.contains("luis=7890"));
+    assertTrue(kvmst2.contains("\n"));
+  }
+}
