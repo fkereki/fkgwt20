ab401d7a2242204da4c1cb4ba8c2510886d04dd8
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2009-10-27 23:57:25 +0000

    Small fixes

--- mvptest/test/com/fkereki/mvptest/client/KeyValueMapTest.java
+++ mvptest/test/com/fkereki/mvptest/client/KeyValueMapTest.java
@@ -1,6 +1,5 @@
 package com.fkereki.mvptest.client;
 
-
 import static org.easymock.classextension.EasyMock.createMock;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.verify;
@@ -13,56 +12,66 @@ import org.junit.Test;
 
 public class KeyValueMapTest {
 
-  @Before
-  public void setUp() throws Exception {
-  }
+	@Before
+	public void setUp() throws Exception {
+	}
 
-  @After
-  public void tearDown() throws Exception {
-  }
 
-  @Test
-  public void testKeyValueMap() {
-    KeyValueMap kvm0 = new KeyValueMap("");
-    assertTrue(kvm0.isEmpty());
-  }
 
-  @Test
-  public void testKeyValueMapString() {
-    KeyValueMap kvm1 = new KeyValueMap("pepe=123456");
-    assertEquals(1, kvm1.size());
+	@After
+	public void tearDown() throws Exception {
+	}
 
-    KeyValueMap kvm2 = new KeyValueMap("pepe=123&juan=456");
-    assertEquals(2, kvm2.size());
-    assertTrue(kvm2.containsKey("pepe"));
-    assertTrue(kvm2.containsKey("juan"));
-    assertEquals("123", kvm2.get("pepe"));
-    assertEquals("456", kvm2.get("juan"));
-  }
 
-  @Test
-  public void testToString() {
-    KeyValueMap kvm0 = new KeyValueMap("");
-    assertEquals("", kvm0.toString());
 
-    KeyValueMap kvm1 = new KeyValueMap("pepe=123456");
-    assertEquals("pepe=123456", kvm1.toString());
+	@Test
+	public void testKeyValueMap() {
+		KeyValueMap kvm0 = new KeyValueMap("");
+		assertTrue(kvm0.isEmpty());
+	}
 
-    KeyValueMap kvm2 = new KeyValueMap(
-      "pepe=123&juan=456&luis=7890");
-    String kvmst2 = kvm2.toString();
-    assertTrue(kvmst2.contains("pepe=123"));
-    assertTrue(kvmst2.contains("juan=456"));
-    assertTrue(kvmst2.contains("luis=7890"));
-    assertTrue(kvmst2.contains("\n"));
-  }
 
-  @Test
-  public void testWithEasyMock() {
-    KeyValueMap kvmMock = createMock(KeyValueMap.class);
-    kvmMock.initializeWithString("pepe=123");
-    replay(kvmMock);
-    kvmMock.initializeWithString("pepe=123");
-    verify(kvmMock);
-  }
+
+	@Test
+	public void testKeyValueMapString() {
+		KeyValueMap kvm1 = new KeyValueMap("pepe=123456");
+		assertEquals(1, kvm1.size());
+
+		KeyValueMap kvm2 = new KeyValueMap("pepe=123&juan=456");
+		assertEquals(2, kvm2.size());
+		assertTrue(kvm2.containsKey("pepe"));
+		assertTrue(kvm2.containsKey("juan"));
+		assertEquals("123", kvm2.get("pepe"));
+		assertEquals("456", kvm2.get("juan"));
+	}
+
+
+
+	@Test
+	public void testToString() {
+		KeyValueMap kvm0 = new KeyValueMap("");
+		assertEquals("", kvm0.toString());
+
+		KeyValueMap kvm1 = new KeyValueMap("pepe=123456");
+		assertEquals("pepe=123456", kvm1.toString());
+
+		KeyValueMap kvm2 = new KeyValueMap(
+		    "pepe=123&juan=456&luis=7890");
+		String kvmst2 = kvm2.toString();
+		assertTrue(kvmst2.contains("pepe=123"));
+		assertTrue(kvmst2.contains("juan=456"));
+		assertTrue(kvmst2.contains("luis=7890"));
+		assertTrue(kvmst2.contains("\n"));
+	}
+
+
+
+	@Test
+	public void testWithEasyMock() {
+		KeyValueMap kvmMock = createMock(KeyValueMap.class);
+		kvmMock.initializeWithString("pepe=123");
+		replay(kvmMock);
+		kvmMock.initializeWithString("pepe=123");
+		verify(kvmMock);
+	}
 }
--- mvptest/test/com/fkereki/mvptest/client/LoginPresenterTest.java
+++ mvptest/test/com/fkereki/mvptest/client/LoginPresenterTest.java
@@ -1,6 +1,5 @@
 package com.fkereki.mvptest.client;
 
-
 import static org.easymock.EasyMock.expect;
 import static org.easymock.classextension.EasyMock.createMock;
 import static org.junit.Assert.assertTrue;
@@ -14,53 +13,53 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public class LoginPresenterTest {
 
-  @Test
-  public void testLoginPresenter1() {
-    GWTMockUtilities.disarm();
-    Model modelMock = createMock(Model.class);
-    LoginView loginViewMock = createMock(LoginView.class);
-    LoginServiceAsync loginServiceMock = new LoginServiceAsync() {
-      @Override
-      public void getSomething(String name, String pass,
-        AsyncCallback<String> callback) {
-        System.out.print("name=" + name + " pass=" + pass);
-      }
-    };
+	@Test
+	public void testLoginPresenter1() {
+		GWTMockUtilities.disarm();
+		Model modelMock = createMock(Model.class);
+		LoginPresenter.PresenterDisplay loginViewMock = createMock(LoginPresenter.PresenterDisplay.class);
+		LoginServiceAsync loginServiceMock = new LoginServiceAsync() {
+			@Override
+			public void getSomething(String name, String pass,
+			    AsyncCallback<String> callback) {
+				System.out.print("name=" + name + " pass=" + pass);
+			}
+		};
 
-    loginViewMock.setName("federico");
-    loginViewMock.setPassword("eduardo");
-    Capture<SimpleCallback<Object>> callbackCapture = new Capture<SimpleCallback<Object>>();
-    loginViewMock.setLoginCallback(EasyMock
-      .capture(callbackCapture));
-    expect(loginViewMock.getName()).andReturn("federico");
-    expect(loginViewMock.getPassword())
-      .andReturn("eduardo");
+		loginViewMock.setName("federico");
+		loginViewMock.setPassword("eduardo");
+		Capture<SimpleCallback<Object>> callbackCapture = new Capture<SimpleCallback<Object>>();
+		loginViewMock.setLoginCallback(EasyMock
+		    .capture(callbackCapture));
+		expect(loginViewMock.getName()).andReturn("federico");
+		expect(loginViewMock.getPassword())
+		    .andReturn("eduardo");
 
-    expect(modelMock.getRemoteLoginService()).andReturn(
-      loginServiceMock);
+		expect(modelMock.getRemoteLoginService()).andReturn(
+		    loginServiceMock);
 
-    SimpleCallback<String> callbackMock = new SimpleCallback<String>() {
-      @Override
-      public void goBack(String result) {
-        System.out.print("result " + result);
-      }
-    };
+		SimpleCallback<String> callbackMock = new SimpleCallback<String>() {
+			@Override
+			public void goBack(String result) {
+				System.out.print("result " + result);
+			}
+		};
 
-    assertTrue(modelMock != null);
-    assertTrue(loginViewMock != null);
-    assertTrue(loginServiceMock != null);
+		assertTrue(modelMock != null);
+		assertTrue(loginViewMock != null);
+		assertTrue(loginServiceMock != null);
 
-    EasyMock.replay(modelMock);
-    EasyMock.replay(loginViewMock);
+		EasyMock.replay(modelMock);
+		EasyMock.replay(loginViewMock);
 
-    LoginPresenter lp = new LoginPresenter(loginViewMock,
-      modelMock, callbackMock);
+		LoginPresenter lp = new LoginPresenter(loginViewMock,
+		    modelMock, callbackMock);
 
-    assertTrue(callbackCapture.getValue() != null);
-    assertTrue(lp.loginService != null);
-    assertTrue(lp.loginService == loginServiceMock);
+		assertTrue(callbackCapture.getValue() != null);
+		assertTrue(lp.loginService != null);
+		assertTrue(lp.loginService == loginServiceMock);
 
-    callbackCapture.getValue().goBack("");
-    GWTMockUtilities.restore();
-  }
+		callbackCapture.getValue().goBack("");
+		GWTMockUtilities.restore();
+	}
 }
