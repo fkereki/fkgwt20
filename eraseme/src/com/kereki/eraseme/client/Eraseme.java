d8321e02b6ab242084f67e7c4120d66aaf532b61
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2009-12-20 22:10:53 +0000

    Experiment with suggestBox

--- eraseme/src/com/kereki/eraseme/client/Eraseme.java
+++ eraseme/src/com/kereki/eraseme/client/Eraseme.java
@@ -1,5 +1,8 @@
 package com.kereki.eraseme.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
@@ -7,15 +10,44 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.DomEvent;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.IsSerializable;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Eraseme implements EntryPoint {
+  public class SuggestionItem implements Suggestion, IsSerializable {
+    private String suggestionText;
+
+    /**
+     * Empty constructor: Required for serialization
+     */
+    public SuggestionItem() {
+    }
+
+    /**
+     * Simplify creating a straight suggestion item
+     */
+    public SuggestionItem(String text) {
+      suggestionText = text;
+    }
+
+    public String getDisplayString() {
+      return suggestionText;
+    }
+
+    public String getReplacementString() {
+      return suggestionText;
+    }
+  }
+
   public void onModuleLoad() {
     final VerticalPanel vp = new VerticalPanel();
     final Button unoButton = new Button("Hace sonar al 2");
@@ -23,6 +55,27 @@ public class Eraseme implements EntryPoint {
 
     final TextBox trestexto = new TextBox();
     final TextBox cuatrotexto = new TextBox();
+
+    MultiWordSuggestOracle oracle = new MultiWordSuggestOracle() {
+      @Override
+      public void requestSuggestions(Request request, Callback callback) {
+        Response resp = new Response();
+        List<SuggestionItem> suggestions = new ArrayList<SuggestionItem>();
+
+        String beginning = request.getQuery();
+        if (beginning.length() > 3) {
+          suggestions.add(new SuggestionItem(beginning
+              + " It is a good day to die"));
+          suggestions.add(new SuggestionItem(beginning + " I shall return"));
+          suggestions.add(new SuggestionItem(beginning
+              + " There is nothing to fear but fear itself"));
+        }
+        resp.setSuggestions(suggestions);
+        callback.onSuggestionsReady(request, resp);
+      }
+    };
+
+    SuggestBox box = new SuggestBox(oracle);
 
     unoButton.addClickHandler(new ClickHandler() {
 
@@ -74,6 +127,7 @@ public class Eraseme implements EntryPoint {
       }
     });
 
+    vp.add(box);
     vp.add(unoButton);
     vp.add(dosButton);
     vp.add(trestexto);
