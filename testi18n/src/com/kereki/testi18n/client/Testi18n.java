e66a02b760c0c67bb344d4692b44315783b67fa9
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-02-15 04:02:59 +0000

    UIBinder i18n stuff

--- testi18n/src/com/kereki/testi18n/client/Testi18n.java
+++ testi18n/src/com/kereki/testi18n/client/Testi18n.java
@@ -1,8 +1,6 @@
 package com.kereki.testi18n.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Window;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -12,21 +10,24 @@ public class Testi18n
 
   @Override
   public void onModuleLoad() {
-    // final Greet greet = GWT.create(Greet.class);
+    // final Greeter greet = GWT.create(Greeter.class);
     //
     // Window.alert(greet.genericHello() + " " + greet.specificQuery());
     //
+    // Window.alert(greet.kindOfUsers());
     // Window.alert(greet.getString("kindOfUsers"));
+    //
+    // final MyMessages myme = GWT.create(MyMessages.class);
+    //
+    // Window.alert(myme.sayGoodbye("Mr Chips"));
+    //
+    // Window.alert(myme.sayAge2(1960, 49));
+    // Window.alert(myme.sayAge2(2008, 1));
+    //
+    // Window.alert(myme.howMany(0) + "/" + myme.howMany(1) + "/"
+    // + myme.howMany(2) + "/" + myme.howMany(3));
 
-    final MyMessages myme = GWT.create(MyMessages.class);
-
-    Window.alert(myme.sayGoodbye("Mr Chips"));
-
-    Window.alert(myme.sayAge2(1960, 49));
-    Window.alert(myme.sayAge2(2008, 1));
-
-    Window.alert(myme.howMany(0) + "/" + myme.howMany(1) + "/"
-        + myme.howMany(2) + "/" + myme.howMany(3));
+    final Example ex = new Example();
 
   }
 }
