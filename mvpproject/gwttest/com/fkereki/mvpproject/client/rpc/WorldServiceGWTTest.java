8f3aa6cfa0225c98f2db9f6bd498e41f0e40b230
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-02-27 23:20:03 +0000

    GWTTestCase examples ready.

---
+++ mvpproject/gwttest/com/fkereki/mvpproject/client/rpc/WorldServiceGWTTest.java
@@ -0,0 +1,55 @@
+package com.fkereki.mvpproject.client.rpc;
+
+import java.util.LinkedHashMap;
+
+import org.junit.Test;
+
+import com.fkereki.mvpproject.client.Model;
+import com.fkereki.mvpproject.client.SimpleCallback;
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class WorldServiceGWTTest
+    extends GWTTestCase {
+
+  final Model model = new Model();
+
+  @Override
+  public String getModuleName() {
+    return "com.fkereki.mvpproject.Mvpproject";
+  }
+
+  @Test
+  public void testGetCities() {
+    model.getCities("UY", "17", 0, 1000,
+        new SimpleCallback<LinkedHashMap<String, ClientCityData>>() {
+          @Override
+          public void goBack(
+              final LinkedHashMap<String, ClientCityData> result) {
+
+            assertTrue(result.size() > 0);
+            assertEquals("Darwin",
+                result.get("darwin").cityAccentedName);
+            finishTest();
+          }
+        });
+
+    assertTrue(true);
+    delayTestFinish(5000);
+  }
+
+  @Test
+  public void testGetCountries() {
+    model
+        .getCountries(new SimpleCallback<LinkedHashMap<String, String>>() {
+          @Override
+          public void goBack(final LinkedHashMap<String, String> result) {
+
+            assertTrue(result.size() > 0);
+            assertTrue(result.containsKey("UY"));
+            finishTest();
+          }
+        });
+
+    delayTestFinish(5000);
+  }
+}
