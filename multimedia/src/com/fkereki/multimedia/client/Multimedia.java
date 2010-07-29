60273e3d0a381b17dfb3c8174fe43b5c56ed293c
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-07-29 05:49:05 +0000

    Primera versión con <audio> funcionando, todo OK.

--- multimedia/src/com/fkereki/multimedia/client/Multimedia.java
+++ multimedia/src/com/fkereki/multimedia/client/Multimedia.java
@@ -5,32 +5,48 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Multimedia implements EntryPoint {
+	HTML audioHtml= new HTML("<audio id='audioelement' src='http://www.sound-effect.com/sounds1/office/TYPING_t.wav'>NO AUDIO?</audio>");
+
 	@Override
 	public void onModuleLoad() {
 		VerticalPanel vp= new VerticalPanel();
-		final Button audioButton= new Button("Play Music");
+		final Button loadAudioButton= new Button("Load Audio");
+		final Button playAudioButton= new Button("Play Audio");
 		final Button videoButton= new Button("Show Video");
-		vp.add(audioButton);
+		vp.add(loadAudioButton);
+		vp.add(playAudioButton);
 		vp.add(videoButton);
 
-		audioButton.addClickHandler(new ClickHandler() {
+		loadAudioButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(final ClickEvent event) {
-				Window.alert("Music!");
+				RootPanel.get().add(audioHtml);
+			}
+		});
+
+		playAudioButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(final ClickEvent event) {
+				playAudio();
 			}
 		});
 
 		videoButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(final ClickEvent event) {
-				Window.alert("Video!");
+				Window.alert("Video!!");
 			}
 		});
 
 		RootPanel.get().add(vp);
 	}
+
+	private native void playAudio() /*-{
+		$doc.getElementById("audioelement").play();
+	}-*/;
 }
