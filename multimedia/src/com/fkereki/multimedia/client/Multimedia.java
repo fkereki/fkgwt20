5d30cb4c32a4dfa86ffc59b403e4a351d6b0e979
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-07-30 02:09:48 +0000

    Versión con dos modos de tocar sonido: directamente vía HTML5, o usando gwt_html5_media

--- multimedia/src/com/fkereki/multimedia/client/Multimedia.java
+++ multimedia/src/com/fkereki/multimedia/client/Multimedia.java
@@ -1,52 +1,44 @@
 package com.fkereki.multimedia.client;
 
+import com.fkereki.multimedia.client.gwtHtml5Media.Audio;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Multimedia implements EntryPoint {
-	HTML audioHtml= new HTML("<audio id='audioelement' src='http://www.sound-effect.com/sounds1/office/TYPING_t.wav'>NO AUDIO?</audio>");
+	// http://www.partnersinrhyme.com/soundfx/Office.shtml
 
 	@Override
 	public void onModuleLoad() {
 		VerticalPanel vp= new VerticalPanel();
-		final Button loadAudioButton= new Button("Load Audio");
-		final Button playAudioButton= new Button("Play Audio");
-		final Button videoButton= new Button("Show Video");
-		vp.add(loadAudioButton);
-		vp.add(playAudioButton);
-		vp.add(videoButton);
 
-		loadAudioButton.addClickHandler(new ClickHandler() {
+		final HTML audio1= new HTML("<audio id='audioelement' src='http://www.sound-effect.com/sounds1/office/TYPING_t.wav'>NO AUDIO?</audio>");
+		RootPanel.get().add(audio1);
+
+		final Button playAudioButton1= new Button("Play Audio Through HTML5 via JSNI");
+		vp.add(playAudioButton1);
+		playAudioButton1.addClickHandler(new ClickHandler() {
 			@Override
-			public void onClick(final ClickEvent event) {
-				RootPanel.get().add(audioHtml);
-			}
+			public native void onClick(final ClickEvent event) /*-{
+		$doc.getElementById("audioelement").play();
+	}-*/;
 		});
 
-		playAudioButton.addClickHandler(new ClickHandler() {
+		final Audio audio2= new Audio("http://www.sound-effect.com/sounds1/office/fax2.wav");
+		final Button playAudioButton2= new Button("Play Audio Through gwt_html5_media");
+		vp.add(playAudioButton2);
+		playAudioButton2.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(final ClickEvent event) {
-				playAudio();
-			}
-		});
-
-		videoButton.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(final ClickEvent event) {
-				Window.alert("Video!!");
+				audio2.play();
 			}
 		});
 
 		RootPanel.get().add(vp);
 	}
 
-	private native void playAudio() /*-{
-		$doc.getElementById("audioelement").play();
-	}-*/;
 }
