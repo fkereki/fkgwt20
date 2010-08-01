142474f11ed529de416ddad80a9667ecaeccca30
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-08-01 04:42:39 +0000

    Versión caminando con Flash también, y probada en Windows

--- multimedia/src/com/fkereki/multimedia/client/Multimedia.java
+++ multimedia/src/com/fkereki/multimedia/client/Multimedia.java
@@ -1,5 +1,7 @@
 package com.fkereki.multimedia.client;
 
+import com.allen_sauer.gwt.voices.client.Sound;
+import com.allen_sauer.gwt.voices.client.SoundController;
 import com.fkereki.multimedia.client.gwtHtml5Media.Audio;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -16,10 +18,15 @@ public class Multimedia implements EntryPoint {
 	public void onModuleLoad() {
 		VerticalPanel vp= new VerticalPanel();
 
-		final HTML audio1= new HTML("<audio id='audioelement' src='http://www.sound-effect.com/sounds1/office/TYPING_t.wav'>NO AUDIO?</audio>");
-		RootPanel.get().add(audio1);
+		// versión Windows:
+		/*
+		 * <embed id="mysound") src="TYPING_t.wav" height="0" width="0" type="application/x-mplayer2" autostart="0"
+		 * loop="0"></embed>
+		 */
 
-		final Button playAudioButton1= new Button("Play Audio Through HTML5 via JSNI");
+		final HTML audio1= new HTML("<audio id='audioelement' src='TYPING_t.wav' />");
+		RootPanel.get().add(audio1);
+		final Button playAudioButton1= new Button("Play Audio (Typing) Through HTML5 via JSNI");
 		vp.add(playAudioButton1);
 		playAudioButton1.addClickHandler(new ClickHandler() {
 			@Override
@@ -28,17 +35,30 @@ public class Multimedia implements EntryPoint {
 	}-*/;
 		});
 
-		final Audio audio2= new Audio("http://www.sound-effect.com/sounds1/office/fax2.wav");
-		final Button playAudioButton2= new Button("Play Audio Through gwt_html5_media");
+		final Audio audio2= new Audio("fax2.wav");
+		final Button playAudioButton2= new Button("Play Audio (Fax) Through gwt_html5_media");
 		vp.add(playAudioButton2);
 		playAudioButton2.addClickHandler(new ClickHandler() {
+
 			@Override
 			public void onClick(final ClickEvent event) {
 				audio2.play();
 			}
 		});
 
-		RootPanel.get().add(vp);
-	}
+		final SoundController soundController= new SoundController();
+		final Sound audio3= soundController.createSound(Sound.MIME_TYPE_AUDIO_MPEG, "telephone-ring-1.mp3");
+		final Button playAudioButton3= new Button("Play Audio (Phone) Through gwt_voices");
+		vp.add(playAudioButton3);
+		playAudioButton3.addClickHandler(new ClickHandler() {
 
+			@Override
+			public void onClick(final ClickEvent event) {
+				audio3.play();
+			}
+		});
+
+		RootPanel.get().add(vp);
+
+	}
 }
