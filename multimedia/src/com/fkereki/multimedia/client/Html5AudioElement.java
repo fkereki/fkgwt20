908018a34268321952d3094ba560666885959b21
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-08-02 04:41:36 +0000

    Versión encapsulando sonido en una clase, y usando JSNI para obtener atributos, hacer play(), etc.

---
+++ multimedia/src/com/fkereki/multimedia/client/Html5AudioElement.java
@@ -0,0 +1,12 @@
+package com.fkereki.multimedia.client;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class Html5AudioElement extends AudioElement {
+	public Html5AudioElement(final String audioUrl) {
+		super(audioUrl);
+		final HTML audio1= new HTML("<audio id='" + generateId() + "' src='" + audioUrl + "' />");
+		RootPanel.get().add(audio1);
+	}
+}
