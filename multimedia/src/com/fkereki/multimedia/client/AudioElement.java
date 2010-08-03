03cd4294ffacacfa223683d35556ef51f45ee65d
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-08-03 03:45:42 +0000

    Comentarios de clase agregados por todos lados.

--- multimedia/src/com/fkereki/multimedia/client/AudioElement.java
+++ multimedia/src/com/fkereki/multimedia/client/AudioElement.java
@@ -1,5 +1,14 @@
 package com.fkereki.multimedia.client;
 
+/**
+ * Define the base class for all audio elements. Since there is no
+ * standard audio class, we don't want to directly work with any
+ * specific classes.
+ * 
+ * Note that this is actually an Adapter pattern; see
+ * http://c2.com/cgi/wiki?AdapterPattern or
+ * http://en.wikipedia.org/wiki/Adapter_pattern for more on this.
+ */
 public abstract class AudioElement {
 	public AudioElement(final String audioUrl) {
 	}
--- multimedia/src/com/fkereki/multimedia/client/GwtVoicesAudioElement.java
+++ multimedia/src/com/fkereki/multimedia/client/GwtVoicesAudioElement.java
@@ -3,6 +3,12 @@ package com.fkereki.multimedia.client;
 import com.allen_sauer.gwt.voices.client.Sound;
 import com.allen_sauer.gwt.voices.client.SoundController;
 
+/**
+ * The gwt-voices library at http://code.google.com/p/gwt-voices/ uses a
+ * small Flash object to actually play sound. The only real problem
+ * might be either a Flash-plugin-less browser, or a browser with an
+ * installed Flash blocker.
+ */
 public class GwtVoicesAudioElement extends AudioElement {
 	final SoundController soundController= new SoundController();
 	final Sound sound;
