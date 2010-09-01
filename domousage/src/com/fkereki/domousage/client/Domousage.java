c9d9dcffb3bb134903705576f3b316cdf182d37f
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-09-01 03:30:10 +0000

    Not working...

--- domousage/src/com/fkereki/domousage/client/Domousage.java
+++ domousage/src/com/fkereki/domousage/client/Domousage.java
@@ -1,12 +1,11 @@
 package com.fkereki.domousage.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
 
 public class Domousage implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		Window.alert("Hello, Dojo!");
 	}
+
 }
