db3708d28db9392becdb76b3949fcff78afcea7e
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-09-07 01:31:17 +0000

    Camina, pero por separado... y dibuja mal en Firefox 3.5?

--- domousage/src/com/fkereki/domousage/client/Domousage.java
+++ domousage/src/com/fkereki/domousage/client/Domousage.java
@@ -1,11 +1,29 @@
 package com.fkereki.domousage.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class Domousage implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
+		Button drawGraphButton= new Button("Click me for a graph");
+		RootPanel.get().add(drawGraphButton);
+
+		drawGraphButton.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(final ClickEvent event) {
+				llamadaJSNI();
+			}
+		});
+
 	}
 
+	public native void llamadaJSNI() /*-{
+		$wnd.drawChart("simplechart");
+	}-*/;
 }
