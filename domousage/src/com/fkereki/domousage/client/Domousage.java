c1e45e8c2f6270d697f14b0a24c3e1ed2638ce1c
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-09-07 02:26:40 +0000

    Versión caminando, pasando un vector generado en GWT/Java

--- domousage/src/com/fkereki/domousage/client/Domousage.java
+++ domousage/src/com/fkereki/domousage/client/Domousage.java
@@ -1,9 +1,12 @@
 package com.fkereki.domousage.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArrayNumber;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Domousage implements EntryPoint {
@@ -13,17 +16,31 @@ public class Domousage implements EntryPoint {
 		Button drawGraphButton= new Button("Click me for a graph");
 		RootPanel.get().add(drawGraphButton);
 
-		drawGraphButton.addClickHandler(new ClickHandler() {
+		HTML html= new HTML();
+		html.setHeight("200px");
+		html.setWidth("400px");
+		html.getElement().setId("newchart");
+		RootPanel.get().add(html);
 
+		final JsArrayNumber vectorcito= (JsArrayNumber) JavaScriptObject.createArray();
+		vectorcito.push(1);
+		vectorcito.push(4);
+		vectorcito.push(2);
+		vectorcito.push(6);
+		vectorcito.push(3);
+		vectorcito.push(21);
+		vectorcito.push(21);
+		vectorcito.push(20);
+
+		drawGraphButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(final ClickEvent event) {
-				llamadaJSNI();
+				llamadaJSNI(vectorcito);
 			}
 		});
-
 	}
 
-	public native void llamadaJSNI() /*-{
-		$wnd.drawChart("simplechart");
+	public native void llamadaJSNI(JsArrayNumber vvvv) /*-{
+		$wnd.drawChart("newchart", vvvv);
 	}-*/;
 }
