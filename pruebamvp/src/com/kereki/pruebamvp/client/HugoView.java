10066f5f0e416e925973ad2fb6bfcc19b116480d
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-12-05 23:43:56 +0000

    Intentando un hyperlink...

--- pruebamvp/src/com/kereki/pruebamvp/client/HugoView.java
+++ pruebamvp/src/com/kereki/pruebamvp/client/HugoView.java
@@ -6,6 +6,8 @@ import com.google.gwt.user.client.ui.IsWidget;
 public interface HugoView extends IsWidget {
 	void setName(String aString);
 
+	void setLink(String destination);
+
 	public interface Presenter {
 		void goTo(Place place);
 	}
