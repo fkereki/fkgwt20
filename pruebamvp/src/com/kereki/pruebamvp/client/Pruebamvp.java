413345675371b0c827d6d16e799434a6bd4c9875
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-12-06 22:51:31 +0000

    Version con menu

--- pruebamvp/src/com/kereki/pruebamvp/client/Pruebamvp.java
+++ pruebamvp/src/com/kereki/pruebamvp/client/Pruebamvp.java
@@ -8,6 +8,9 @@ import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.place.shared.PlaceHistoryHandler;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 
@@ -18,11 +21,13 @@ public class Pruebamvp implements EntryPoint {
 	private final Place defaultPlace= new HugoPlace("defaultdehugo");
 	private final SimplePanel appWidget= new SimplePanel();
 
+	private final MenuBar menuBar= new MenuBar();
+
 	@Override
 	public void onModuleLoad() {
-		Environment environment= new Environment();
-		EventBus eventBus= environment.eventBus;
-		PlaceController placeController= environment.placeController;
+		final Environment environment= new Environment();
+		final EventBus eventBus= environment.eventBus;
+		final PlaceController placeController= environment.placeController;
 
 		ActivityMapper activityMapper= new AppActivityMapper(environment);
 		ActivityManager activityManager= new ActivityManager(activityMapper, eventBus);
@@ -31,6 +36,31 @@ public class Pruebamvp implements EntryPoint {
 		AppPlaceHistoryMapper historyMapper= GWT.create(AppPlaceHistoryMapper.class);
 		PlaceHistoryHandler historyHandler= new PlaceHistoryHandler(historyMapper);
 		historyHandler.register(placeController, eventBus, defaultPlace);
+
+		MenuItem menuHugo= new MenuItem("Hugo", new Command() {
+			@Override
+			public void execute() {
+				environment.placeController.goTo(new HugoPlace("hugostring1"));
+			}
+		});
+
+		MenuItem menuPaco= new MenuItem("Paco", new Command() {
+			@Override
+			public void execute() {
+				environment.placeController.goTo(new PacoPlace("Pacostring2"));
+			}
+		});
+
+		MenuItem menuLuis= new MenuItem("Luis", new Command() {
+			@Override
+			public void execute() {
+				environment.placeController.goTo(new LuisPlace("Luisstring3"));
+			}
+		});
+
+		menuBar.addItem(menuHugo);
+		menuBar.addItem(menuPaco);
+		menuBar.addItem(menuLuis);
 
 		RootPanel.get().add(appWidget);
 		historyHandler.handleCurrentHistory();
