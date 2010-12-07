904562325fa03d6a7a6a51786ce48cf89a5824cd
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-12-07 01:51:42 +0000

    Prueba con una form que sume dos números, en modo MVP

--- pruebamvp/src/com/kereki/pruebamvp/client/PacoActivity.java
+++ pruebamvp/src/com/kereki/pruebamvp/client/PacoActivity.java
@@ -8,6 +8,7 @@ import com.google.gwt.user.client.ui.AcceptsOneWidget;
 public class PacoActivity extends AbstractActivity implements PacoView.Presenter {
 	private final Environment environment;
 	private final Place place;
+	private PacoView pv;
 
 	public PacoActivity(final Place place, final Environment environment) {
 		this.place= place;
@@ -16,13 +17,20 @@ public class PacoActivity extends AbstractActivity implements PacoView.Presenter
 
 	@Override
 	public void start(final AcceptsOneWidget panel, final EventBus eventBus) {
-		PacoView pv= environment.getNewPacoView();
-		pv.setSomething("tarzan king jungle!!");
+		pv= environment.getNewPacoView();
+		pv.setPresenter(this);
 		panel.setWidget(pv);
 	}
 
 	@Override
 	public void goTo(final Place place) {
 		environment.placeController.goTo(place);
+	}
+
+	@Override
+	public void onSumaClick() {
+		int a= pv.get1stNumber();
+		int b= pv.get2ndNumber();
+		pv.setSum(a + b);
 	}
 }
--- pruebamvp/src/com/kereki/pruebamvp/client/PacoView.java
+++ pruebamvp/src/com/kereki/pruebamvp/client/PacoView.java
@@ -4,12 +4,18 @@ import com.google.gwt.place.shared.Place;
 import com.google.gwt.user.client.ui.IsWidget;
 
 public interface PacoView extends IsWidget {
-	void setSomething(String aString);
+	public void setPresenter(final PacoView.Presenter presenter);
 
-	String getSomething();
+	public int get1stNumber();
+
+	public int get2ndNumber();
+
+	public void setSum(int aNumber);
 
 	public interface Presenter {
 		void goTo(Place place);
+
+		void onSumaClick();
 	}
 
 }
