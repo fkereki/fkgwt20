bdea5b1812d97c4a2a64b65aa1555aa70de76240
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-12-07 01:51:42 +0000

    Prueba con una form que sume dos números, en modo MVP

--- pruebamvp/src/com/kereki/pruebamvp/client/PacoViewImpl.java
+++ pruebamvp/src/com/kereki/pruebamvp/client/PacoViewImpl.java
@@ -1,25 +1,60 @@
 package com.kereki.pruebamvp.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class PacoViewImpl extends Composite implements PacoView {
-	HorizontalPanel hp= new HorizontalPanel();
-	Label lb= new Label();
+	PacoView.Presenter presenter;
+
+	FlexTable ft= new FlexTable();
+	Label lbl1= new Label("1r número:");
+	Label lbl2= new Label("2o número:");
+	TextBox tb1= new TextBox();
+	TextBox tb2= new TextBox();
+	Button btn= new Button("Sumar:");
+	TextBox tb3= new TextBox();
 
 	public PacoViewImpl() {
-		hp.add(lb);
-		initWidget(hp);
+		tb3.setReadOnly(true);
+
+		ft.setWidget(0, 0, lbl1);
+		ft.setWidget(0, 1, tb1);
+		ft.setWidget(1, 0, lbl2);
+		ft.setWidget(1, 1, tb2);
+		ft.setWidget(2, 0, btn);
+		ft.setWidget(2, 1, tb3);
+		initWidget(ft);
 	}
 
 	@Override
-	public void setSomething(final String aString) {
-		lb.setText(aString);
+	public void setPresenter(final Presenter presenter) {
+		this.presenter= presenter;
+
+		btn.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(final ClickEvent event) {
+				presenter.onSumaClick();
+			}
+		});
 	}
 
 	@Override
-	public String getSomething() {
-		return lb.getText() + "//" + lb.getText();
+	public int get1stNumber() {
+		return Integer.parseInt(tb1.getValue());
+	}
+
+	@Override
+	public int get2ndNumber() {
+		return Integer.parseInt(tb2.getValue());
+	}
+
+	@Override
+	public void setSum(final int aNumber) {
+		tb3.setValue("" + aNumber);
 	}
 }
