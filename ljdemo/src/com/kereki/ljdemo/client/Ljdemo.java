bf907892a45ee1f6c49953fddecdafd4fb4e7993
fkereki <fkereki@1886bbd2-b466-11de-8ddf-618d6b694b25>
2010-12-15 02:04:09 +0000

    Empty project

---
+++ ljdemo/src/com/kereki/ljdemo/client/Ljdemo.java
@@ -0,0 +1,11 @@
+package com.kereki.ljdemo.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class Ljdemo implements EntryPoint {
+	@Override
+	public void onModuleLoad() {
+		Window.alert("Hi there!");
+	}
+}
