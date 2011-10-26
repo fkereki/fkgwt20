ca5d8b503d1b048caaf905ae5d4d8bf18338d824
fkereki@gmail.com <fkereki@gmail.com@1886bbd2-b466-11de-8ddf-618d6b694b25>
2011-10-26 01:53:07 +0000

    Reformateo

--- mvpproject/XmlJsonTest/src/com/google/gwt/kereki/xmlJsonTest/client/GeoDataV3.java
+++ mvpproject/XmlJsonTest/src/com/google/gwt/kereki/xmlJsonTest/client/GeoDataV3.java
@@ -8,31 +8,31 @@ public class GeoDataV3 extends JavaScriptObject {
 
     public final native String getAddressPartsLongName(final int i)
     /*-{
-		return this.results[0].address_components[i].long_name;
+    	return this.results[0].address_components[i].long_name;
     }-*/;
 
     public final native int getAddressPartsLength()
     /*-{
-		return this.results[0].address_components.length;
+    	return this.results[0].address_components.length;
     }-*/;
 
     public final native String getAddressPartsType(final int i, final int j)
     /*-{
-		return this.results[0].address_components[i].types[j];
+    	return this.results[0].address_components[i].types[j];
     }-*/;
 
     public final native int getAddressPartsTypeLength(final int i)
     /*-{
-		return this.results[0].address_components[i].types.length;
+    	return this.results[0].address_components[i].types.length;
     }-*/;
 
     public final native float getLatitude()
     /*-{
-		return this.results[0].geometry.location.lat;
+    	return this.results[0].geometry.location.lat;
     }-*/;
 
     public final native float getLongitude()
     /*-{
-		return this.results[0].geometry.location.lng;
+    	return this.results[0].geometry.location.lng;
     }-*/;
 }
