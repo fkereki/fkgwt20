523641208650456aff727313ab252475e9eac062
fkereki@gmail.com <fkereki@gmail.com@1886bbd2-b466-11de-8ddf-618d6b694b25>
2011-10-26 01:53:07 +0000

    Reformateo

--- mvpproject/XmlJsonTest/src/com/google/gwt/kereki/xmlJsonTest/client/GeoDataV2.java
+++ mvpproject/XmlJsonTest/src/com/google/gwt/kereki/xmlJsonTest/client/GeoDataV2.java
@@ -8,26 +8,26 @@ public class GeoDataV2 extends JavaScriptObject {
 
     public final native String getAddress()
     /*-{
-		return this.Placemark[0].address;
+    	return this.Placemark[0].address;
     }-*/;
 
     public final native String getLocality()
     /*-{
-		return this.Placemark[0].AddressDetails.Country.AdministrativeArea.Locality.LocalityName;
+    	return this.Placemark[0].AddressDetails.Country.AdministrativeArea.Locality.LocalityName;
     }-*/;
 
     public final native String getPostalCode()
     /*-{
-		return this.Placemark[0].AddressDetails.Country.AdministrativeArea.Locality.PostalCode.PostalCodeNumber;
+    	return this.Placemark[0].AddressDetails.Country.AdministrativeArea.Locality.PostalCode.PostalCodeNumber;
     }-*/;
 
     public final native float getLatitude()
     /*-{
-		return this.Placemark[0].Point.coordinates[1];
+    	return this.Placemark[0].Point.coordinates[1];
     }-*/;
 
     public final native float getLongitude()
     /*-{
-		return this.Placemark[0].Point.coordinates[0];
+    	return this.Placemark[0].Point.coordinates[0];
     }-*/;
 }
